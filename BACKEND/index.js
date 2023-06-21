574ca025b896b8fbeffd780338b708bc04f23dee
Sidd1600 <135330916+Sidd1600@users.noreply.github.com>
2023-06-22 02:00:44 +0530

    Updated index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,17 +1,21 @@
+// IMPORTING REQUIRED MODULES
 const mongoose = require('mongoose');
-let canteenmenu=require("./model.js");
-const bodyParser=require("body-parser")
+const bodyParser = require("body-parser")
 const ejs = require("ejs");
+const express = require('express');
+const path = require('path');
+const app = express();
 
+// SELECTING THE PORT
+const port = 80;
 
-const express=require('express');
-const app=express();
-const path=require('path');
-const port=80;
+// IMPORTING SCHEMA OF FOOD ITEMS FROM "model.js"
+let canteenmenu = require("./model.js");
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-// import canteenmenu from "./model.js";
+
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -19,106 +23,114 @@ app.use(bodyParser.urlencoded({
 
 main().catch(err => console.log(err));
 
+
+// CONNECTING MONGOOSE AND MONGODB
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/canteen');
-    console.log("WE ARE NOW CONNECTED");
+  console.log("WE ARE NOW CONNECTED");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-// const canteenmenu = new mongoose.Schema({
-//    name: String,
-//     price: Number,
-//     description: String,
-//     id:String
-//   });
 
-  const Menu = mongoose.model('Menu', canteenmenu);
+// MODELLING OUR MENU
+const Menu = mongoose.model('Menu', canteenmenu);
 
-  
-  const menuitems = Menu.find({}).then((err, posts) => {
-    if (err) {
-        console.error(err);
-      } else {
-        console.log(posts);
-      }
-    });
+// DISPLAYING ALL FOOD ITEMS AVAILABLE IN OUR MENU
+const menuitems = Menu.find({}).then((err, posts) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(posts);
+  }
+});
 console.log(menuitems);
 
-app.get("/canteen",function(req,res){
+
+// ANSWERING A GET REQUEST 
+app.get("/canteen", function (req, res) {
   Menu.find({}).then((err, posts) => {
     if (err) {
-        res.send(err);
-      } else {
-        res.send(posts);
-      }
-    });
+      res.send(err);
+    } else {
+      res.send(posts);
+    }
+  });
 });
 
-app.post("/canteen",function(req,res){
-  const newitem=new Menu({
-    name : req.body.name,
-    description:req.body.description,
-    price:req.body.price,
-    id:req.body.id
+// ANSWERING A POST REQUEST 
+app.post("/canteen", function (req, res) {
+  const newitem = new Menu({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    id: req.body.id
   });
-
-  
   // console.log(req.body);
   newitem.save();
 });
-app.get("/canteen/:nitem",function(req,res){
+
+// ANSWERING A GET REQUEST 
+app.get("/canteen/:nitem", function (req, res) {
   Menu.findOne({
-    name:req.params.nitem
-  }).then((err, posts) =>{
+    name: req.params.nitem
+  }).then((err, posts) => {
     if (err) {
       res.send(err);
-      } else {
-        res.send("NO ARTICLE FOUND HERE");
-      }
+    } else {
+      res.send("NO ARTICLE FOUND HERE");
+    }
   });
 });
 
-app.put("/canteen/:nitem",function(req,res){
+// ANSWERING A PUT REQUEST 
+app.put("/canteen/:nitem", function (req, res) {
   Menu.updateOne(
-    {name:req.params.nitem},
-    {$set:{name:req.body.name,
-    price:req.body.price,
-    description:req.body.description,
-    id:req.body.id}},
-    {overwrite:true}).then(
-    function(err){
-      if(err){
-        res.send(err);
-      }else{
-        res.send("SUCCESSFULLY UPDATED");
+    { name: req.params.nitem },
+    {
+      $set: {
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        id: req.body.id
       }
-    }
+    },
+    { overwrite: true }).then(
+      function (err) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send("SUCCESSFULLY UPDATED");
+        }
+      }
     );
-        })
+})
 
-        app.patch("/canteen/:nitem",function(req,res){
-          Menu.updateOne(
-            {name:req.params.nitem},
-            {$set:req.body}).then(
-            function(err){
-              if(err){
-                res.send(err);
-              }else{
-                res.send("SUCCESSFULLY UPDATED");
-              }
-            }
-            );
-                });
-                app.delete("/canteen/:nitem",function(req,res){
-                  Menu.deleteOne({name:req.params.nitem}).then(function(err){
-                    if(err){
-                      res.send("DELETED SUCCESSFULY");
-                    }else{
-                      res.send(err);
-                    }
-                  })
-                   
-                    
-                })
+// ANSWERING A PATCH REQUEST 
+app.patch("/canteen/:nitem", function (req, res) {
+  Menu.updateOne(
+    { name: req.params.nitem },
+    { $set: req.body }).then(
+      function (err) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send("SUCCESSFULLY UPDATED");
+        }
+      }
+    );
+});
+
+// ANSWERING A DELETE REQUEST 
+app.delete("/canteen/:nitem", function (req, res) {
+  Menu.deleteOne({ name: req.params.nitem }).then(function (err) {
+    if (err) {
+      res.send("DELETED SUCCESSFULY");
+    } else {
+      res.send(err);
+    }
+  })
+
+
+})
 // Menu.fin;d({}, (err, posts) => {
 //     if (err) {
 //       console.error(err);
@@ -126,7 +138,8 @@ app.put("/canteen/:nitem",function(req,res){
 //       console.log(posts);
 //     }
 //   });
-  
-app.listen(port,()=>{
+
+
+app.listen(port, () => {
   console.log(`THE APPLICATION STARTED SUCCESSFULLY ON PORT ${port}`);
 });
